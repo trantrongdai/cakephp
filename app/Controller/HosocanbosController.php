c402a8131b7bc90a14ae7521c4186468d166d863
trantrongdai <trongdai306@gmail.com>
2014-01-20 10:52:08 +0700

    loloadCreate README.md

--- app/Controller/HosocanbosController.php
+++ app/Controller/HosocanbosController.php
@@ -1,10 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 class HosocanbosController extends AppController {
 
@@ -17,7 +12,8 @@ class HosocanbosController extends AppController {
             
         } else {
             $hosocanbos = $this->Hosocanbo->find('all', array(
-                'conditions' => array('Hosocanbo.hoten LIKE' => '%' . $data['Hosocanbo']['Tên'] . '%')));
+                'conditions' => array('Hosocanbo.hoten LIKE' => '%' . $data['Hosocanbo']['Tên'] . '%',
+                    'Hosocanbo.gioitinh = '=> $data['Hosocanbo']['Giới tính'] == 'Nam')));
         }
         $this->set('hosocanbos', $hosocanbos);
 
